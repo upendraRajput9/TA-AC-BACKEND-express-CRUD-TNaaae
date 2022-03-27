caae4b5c39b629dd7d65636ed8f7999d8fa2a53e
upendra <upendrarajput9911@gmail.com>
2022-03-27 18:07:11 +0530

    done

--- block-BNaacu/school/routes/index.js
+++ block-BNaacu/school/routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express')
 var mongoose = require('mongoose')
 var path = require('path')
-const students = require('../models/students')
 var Student = require('../models/students')
 var router = express.Router()
 
@@ -13,13 +12,18 @@ mongoose.connect('mongodb://localhost/routes', (err) => {
 router.use(express.json())
 router.use(express.urlencoded({ extended: false }))
 //student routes
-router.use(express.static(path.join(__dirname, '../public')));
+router.use(express.static(path.join("../",__dirname, 'public')));
 
 
 // router
-router.get('/new', (req, res) => {
-  res.sendFile(path.join(__dirname, '../index.html'))
+router.get("/new",(req,res)=>{
+  res.render("studentform")
 })
+
+router.get('/', (req, res) => {
+  res.render("students", { list: ["ankit", "suraj", "prashant", "ravi"] });
+});
+
 
 router.post('/', (req, res) => {
   Student.create(req.body, (err, student) => {
@@ -30,8 +34,8 @@ router.post('/', (req, res) => {
 router.get("/:id",(req,res)=>{
   let id = req.params.id
   Student.findById(id,(err,student)=>{
-    res.render("students", {
-      student: { name: "rahul", email: "rahul@altcampus.io" },
+    res.render("studentDetail", {
+      student: student,
     });
   })
 })
